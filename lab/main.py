c25319b1a38cce0dee803d75ffe6ef0d4c01ce72
garielM01u <gmanzano@miners.utep.edu>
2021-05-14 05:57:10 -0600

    Debugging 1

--- lab/main.py
+++ lab/main.py
@@ -3,42 +3,56 @@
 import os,time,cv2
 from threading import Thread, Semaphore,Lock
 
-count = 0
+
 fileName = '../clip.mp4'
 
 def extract_frames(in_q):
+    count = 0
     vidcap = cv2.VideoCapture(fileName)
     
     while True:
         success, image = vidcap.read() 
         in_q.push(image)
+
+        print("Extracted frame# " + str(count))
+
         if success and count < 99:
             break
         count += 1
+    in_q.push(None)
+    print("Finished extracting")
 
 def convert_frames(in_q, out_q):
-
+    count = 0
     while True:
         frame = in_q.pop()
         if frame is None:
             out_q.push(None)
             break
         conv_f = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        print("Converted frame# "+str(count))
+        count += 1
         out_q.push(conv_f)
+    print("Finished grayscale conversion")
 
 
 def disply_frames(out_q):
+    count = 0
     while True:
         frame = out_q.pop()
         cv2.imshow('Video', frame)
+        print("Displaying frame# " + str(count))
         if cv2.waitKey(42) and 0xFF == ord("q"):
             break
+        count += 1
     cv2.destroyAllWindows()
+    print("Display finished")
 
 
 class PCQueue():
 
-    def init(self, cap):
+    def __init__(self, cap):
         self.full = Semaphore(0)
         self.empty = Semaphore(cap)
         self.queue = list() 
